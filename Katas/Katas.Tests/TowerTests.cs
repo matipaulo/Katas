d43d0049de58a2b3074709a0b1389adff77f5b94
Matias <matipaulo@hotmail.com>
2019-07-31 09:20:07 -0300

    Tower kata

---
+++ Katas/Katas.Tests/TowerTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace Katas.Tests
+{
+    public class TowerTests
+    {
+        [Fact]
+        public void BasicTests()
+        {
+            Assert.Equal(string.Join(",", new[] { "*" }), string.Join(",", Tower.TowerBuilder(1)));
+            Assert.Equal(string.Join(",", new[] { " * ", "***" }), string.Join(",", Tower.TowerBuilder(2)));
+            Assert.Equal(string.Join(",", new[] { "  *  ", " *** ", "*****" }), string.Join(",", Tower.TowerBuilder(3)));
+        }
+    }
+}
---
+++ Katas/Katas/Tower.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace Katas
+{
+    /// <summary>
+    /// for example, a tower of 3 floors looks like below
+    /// 
+    /// [
+    /// '  *  ', 
+    /// ' *** ', 
+    /// '*****'
+    /// ]
+    /// and a tower of 6 floors looks like below
+    /// 
+    /// [
+    /// '     *     ', 
+    /// '    ***    ', 
+    /// '   *****   ', 
+    /// '  *******  ', 
+    /// ' ********* ', 
+    /// '***********'
+    /// ]
+    /// </summary>
+    public static class Tower
+    {
+        public static string[] TowerBuilder(int floors)
+        {
+            if (floors == 0)
+                return new string[0];
+
+            var tower = new List<string>();
+            for (var i = 0; i < floors; i++)
+            {
+                var floor = string.Empty;
+                var asterisk = ((2 * floors) - 1) - (i * 2);
+                floor = floor.PadLeft(asterisk, '*');
+
+                var left = floor.PadLeft(asterisk + i);
+                tower.Add(left.PadRight(left.Length + i));
+            }
+
+            tower.Reverse(0, tower.Count);
+
+            return tower.ToArray();
+        }
+    }
+}
