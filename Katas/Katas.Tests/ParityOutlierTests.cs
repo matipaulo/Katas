23bae1adb575ea15d8c7e35c1d3f9be876de7514
Matias <matipaulo@hotmail.com>
2019-07-31 18:24:42 -0300

    Parity outlier kata

---
+++ Katas/Katas.Tests/ParityOutlierTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace Katas.Tests
+{
+    public class ParityOutlierTests
+    {
+        [Fact]
+        public static void Test1()
+        {
+            int[] exampleTest1 = { 2, 6, 8, -10, 3 };
+            Assert.Equal(3, ParityOutlier.Find(exampleTest1));
+        }
+
+        [Fact]
+        public static void Test2()
+        {
+            int[] exampleTest2 = { 206847684, 1056521, 7, 17, 1901, 21104421, 7, 1, 35521, 1, 7781 };
+            Assert.Equal(206847684, ParityOutlier.Find(exampleTest2));
+        }
+
+        [Fact]
+        public static void Test3()
+        {
+            int[] exampleTest3 = { int.MaxValue, 0, 1 };
+            Assert.Equal(0, ParityOutlier.Find(exampleTest3));
+        }
+    }
+}
---
+++ Katas/Katas/ParityOutlier.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace Katas
+{
+    /// <summary>
+    /// You are given an array (which will have a length of at least 3, but could be very large) containing integers.
+    /// The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N.
+    /// Write a method that takes the array as an argument and returns this "outlier" N.
+    /// </summary>
+    public static class ParityOutlier
+    {
+        public static int Find(int[] integers)
+        {
+            if (integers == null || integers.Length < 3)
+                return -1;
+
+            var isOdd = integers.Count(x => x % 2 != 0) > 1;
+            return isOdd ? integers.FirstOrDefault(x => x % 2 == 0) : integers.FirstOrDefault(x => x % 2 != 0);
+        }
+    }
+}
