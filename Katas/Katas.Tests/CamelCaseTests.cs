57ee91ab99973fb27f80e3c00dd491f3ec321c63
Matias <matipaulo@hotmail.com>
2019-07-31 09:42:22 -0300

    CamelCase kata

---
+++ Katas/Katas.Tests/CamelCaseTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace Katas.Tests
+{
+    public class CamelCaseTests
+    {
+        [Fact]
+        public void BasicTests()
+        {
+            Assert.Equal("TestCase", "test case".ToCamelCase());
+            Assert.Equal("CamelCaseMethod", "camel case method".ToCamelCase());
+            Assert.Equal("SayHello", "say hello".ToCamelCase());
+            Assert.Equal("CamelCaseWord", " camel case word".ToCamelCase());
+            Assert.Equal("", "".ToCamelCase());
+        }
+
+        [Fact]
+        public void ImprovedSolution()
+        {
+            Assert.Equal("TestCase", "test case".ToImprovedCamelCase());
+            Assert.Equal("CamelCaseMethod", "camel case method".ToImprovedCamelCase());
+            Assert.Equal("SayHello", "say hello".ToImprovedCamelCase());
+            Assert.Equal("CamelCaseWord", " camel case word".ToImprovedCamelCase());
+            Assert.Equal("", "".ToImprovedCamelCase());
+        }
+    }
+}
---
+++ Katas/Katas/CamelCase.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+using System.Linq;
+
+namespace Katas
+{
+    public static class CamelCase
+    {
+        public static string ToCamelCase(this string input)
+        {
+            var result = string.Empty;
+            if (string.IsNullOrEmpty(input))
+                return result;
+
+            var words = input.Split(' ').Where(x => x.Length > 0).ToArray();
+            foreach (var word in words)
+            {
+                var characters = word.ToCharArray();
+                result += characters[0].ToString().ToUpperInvariant();
+                for (var i = 1; i < characters.Length; i++)
+                {
+                    result += characters[i];
+                }
+            }
+
+            return result;
+        }
+
+        public static string ToImprovedCamelCase(this string input)
+        {
+            var result = string.Empty;
+            if (string.IsNullOrEmpty(input))
+                return result;
+
+            var textInfo = new CultureInfo("es-ar").TextInfo;
+            result = textInfo.ToTitleCase(input);
+
+            return result.Replace(" ", "");
+        }
+    }
+}
